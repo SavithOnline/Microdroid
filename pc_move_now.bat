c4640760867ea95bee59b1f45c0636b57c23fa8e
SavithOnline <SavithOnline@gmail.com>
2026-09-17 18:54:57 +0530

    Remove pc_move_now.bat helper script

--- pc_move_now.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Microdroid PC control - run an automation on the connected phone.
-REM Usage: pc_move_now.bat [automation_id]   (default: all enabled)
-setlocal
-set DEVICE=R9AMA0LCXEJ
-set ID=%1
-if "%ID%"=="" set ID=all
-adb -s %DEVICE% shell am broadcast -a com.microdroid.ACTION_RUN -n com.example.screenshotmover/.core.MicrodroidReceiver --es automation_id %ID%
-pause
